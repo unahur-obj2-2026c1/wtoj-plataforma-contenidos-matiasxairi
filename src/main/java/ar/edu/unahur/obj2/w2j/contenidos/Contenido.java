75c0c5ffcc0d915d9a7338b67e5130f8ea183f42
matiasxairi <matiasxairi90@gmail.com>
2026-05-27 18:36:09 -0300

    cree la clase contenido

---
+++ src/main/java/ar/edu/unahur/obj2/w2j/contenidos/Contenido.java
@@ -0,0 +1,6 @@
+package ar.edu.unahur.obj2.w2j.contenidos;
+
+public abstract class Contenido{
+
+    public String titulo;
+}
---
+++ src/main/java/ar/edu/unahur/obj2/w2j/contenidos/Pelicula.java
@@ -0,0 +1,5 @@
+package ar.edu.unahur.obj2.w2j.contenidos;
+
+public class Pelicula {
+
+}
